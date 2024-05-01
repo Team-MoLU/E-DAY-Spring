24ce7c4bc4baec2f57f32f784d5f5554a17c1061
easyoungcode <gchun910@gmail.com>
2024-05-01 18:10:11 +0900

    init: 프로젝트 생성

---
+++ src/main/java/team/molu/edayserver/EdayServerApplication.java
@@ -0,0 +1,13 @@
+package team.molu.edayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/molu/edayserver/EdayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.molu.edayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
