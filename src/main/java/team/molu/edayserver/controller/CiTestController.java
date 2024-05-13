25da75fb33d96200de0b919fc20e36403d3c9dac
Haneol <haneolj@gmail.com>
2024-05-13 17:39:55 +0900

    [ci-test] feat: api 테스트용 코드 추가

--- src/main/java/team/molu/edayserver/controller/CiTestController.java
+++ src/main/java/team/molu/edayserver/controller/CiTestController.java
@@ -1,2 +1,13 @@
-package team.molu.edayserver.controller;public class CiTestController {
+package team.molu.edayserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CiTestController {
+
+    @GetMapping("/test")
+    public String TestApi() {
+        return "api connection is complete.";
+    }
 }
