08c6e766fe803a24602291de95bd0254193b123f
Haneol <haneolj@gmail.com>
2024-05-17 11:55:56 +0900

    [EASY-45] refactor: @Autowired 주입 대신 @RequiredArgsConstructor를 사용하도록 수정

--- src/test/java/team/molu/edayserver/EdayServerApplicationTests.java
+++ src/test/java/team/molu/edayserver/EdayServerApplicationTests.java
@@ -1,15 +1,14 @@
 package team.molu.edayserver;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import team.molu.edayserver.repository.UserRepository;
 
 @SpringBootTest
+@RequiredArgsConstructor
 class EdayServerApplicationTests {
 
-    @Autowired
     private UserRepository userRepository;
 
     @Test
