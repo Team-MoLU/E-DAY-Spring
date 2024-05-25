033beca49d9f98d7d248b80f960e9ee6f67514e3
Haneol <haneolj@gmail.com>
2024-05-25 23:16:15 +0900

    [EASY-48] feat: TaskRepository 추가

---
+++ src/main/java/team/molu/edayserver/repository/TaskRepository.java
@@ -0,0 +1,17 @@
+package team.molu.edayserver.repository;
+
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import team.molu.edayserver.domain.Task;
+
+public interface TaskRepository extends ReactiveNeo4jRepository<Task, String> {
+    // email로 root 노드들 조회
+    @Query("MATCH (u:User)-[CREATED_BY]->(r:Task), (r)-[BELONGS_TO]->(t:Task) WHERE u.email = $email RETURN t")
+    Flux<Task> findRootTasks(String email);
+
+    // id로 자식 노드들 조회
+    @Query("MATCH (parentTask:Task {id: $taskId})-[:BELONGS_TO]->(childTask:Task) RETURN childTask")
+    Flux<Task> findChildTasks(String taskId);
+}
