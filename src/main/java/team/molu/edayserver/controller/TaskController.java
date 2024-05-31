8f9a8a7d555eccb51fbe0e765b01684188ccb6f8
Haneol <haneolj@gmail.com>
2024-05-31 14:56:11 +0900

    [EASY-48] feat: TaskController API 라우트 매핑 변경

--- src/main/java/team/molu/edayserver/controller/TaskController.java
+++ src/main/java/team/molu/edayserver/controller/TaskController.java
@@ -5,19 +5,20 @@ import org.springframework.web.bind.annotation.*;
 import team.molu.edayserver.dto.TasksDto;
 import team.molu.edayserver.service.TaskService;
 
-@RestController
+@RestController()
+@RequestMapping("api/v1/tasks")
 public class TaskController {
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) { this.taskService = taskService; }
 
-    @GetMapping("tasks/roots")
+    @GetMapping("/roots")
     public ResponseEntity<TasksDto.SearchTasksResponse> findRootTasks(@RequestParam String email) {
         TasksDto.SearchTasksResponse tasks = taskService.findTaskByRoot(email);
         return ResponseEntity.ok(tasks);
     }
 
-    @GetMapping("a-task-detail/{taskId}")
+    @GetMapping("/{taskId}/subtasks")
     public ResponseEntity<TasksDto.SearchTasksResponse> findChildTasks(@PathVariable String taskId) {
         TasksDto.SearchTasksResponse tasks = taskService.findTaskById(taskId);
         return ResponseEntity.ok(tasks);
