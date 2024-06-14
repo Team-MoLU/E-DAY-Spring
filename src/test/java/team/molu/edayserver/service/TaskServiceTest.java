61a0bcc745255f3633bd7655f4b51816ba7f13db
Haneol <haneolj@gmail.com>
2024-06-14 14:37:27 +0900

    [EASY-53] fix: email 관련 리팩토링에 맞춰 테스트 코드 수정

--- src/test/java/team/molu/edayserver/service/TaskServiceTest.java
+++ src/test/java/team/molu/edayserver/service/TaskServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,7 @@ import team.molu.edayserver.domain.Task;
 import team.molu.edayserver.dto.TasksDto;
 import team.molu.edayserver.exception.TaskNotFoundException;
 import team.molu.edayserver.repository.TaskRepository;
+import team.molu.edayserver.util.SecurityUtils;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -39,13 +41,17 @@ class TaskServiceTest {
         );
         when(taskRepository.findRootTasks(email)).thenReturn(Flux.fromIterable(tasks));
 
-        // When
-        TasksDto.SearchTasksResponse result = taskService.findTaskByRoot(email);
+        try (MockedStatic<SecurityUtils> utilities = mockStatic(SecurityUtils.class)) {
+            utilities.when(SecurityUtils::getAuthenticatedUserEmail).thenReturn(email);
 
-        // Then
-        assertEquals("Task List Size Test",2, result.getTaskList().size());
-        assertEquals("Task1 Properties Test", "Task 1", result.getTaskList().get(0).getName());
-        verify(taskRepository, times(1)).findRootTasks(email);
+            // When
+            TasksDto.SearchTasksResponse result = taskService.findTaskByRoot();
+
+            // Then
+            assertEquals("Task List Size Test", 2, result.getTaskList().size());
+            assertEquals("Task1 Properties Test", "Task 1", result.getTaskList().get(0).getName());
+            verify(taskRepository, times(1)).findRootTasks(email);
+        }
     }
 
     @Test
@@ -100,17 +106,17 @@ class TaskServiceTest {
         String taskId = "1";
         List<TasksDto.TaskRouteDto> routes = Arrays.asList(
                 TasksDto.TaskRouteDto.builder()
-                        .id("1")
+                        .taskId("1")
                         .name("Task 1")
                         .order(1)
                         .build(),
                 TasksDto.TaskRouteDto.builder()
-                        .id("2")
+                        .taskId("2")
                         .name("Task 2")
                         .order(2)
                         .build(),
                 TasksDto.TaskRouteDto.builder()
-                        .id("3")
+                        .taskId("3")
                         .name("Task 3")
                         .order(0)
                         .build()
@@ -142,13 +148,17 @@ class TaskServiceTest {
         Task createdTask = new Task("1", "Task 1", "Memo 1", LocalDateTime.now(), LocalDateTime.now().plusDays(1), 1, false);
         when(taskRepository.createTaskWithRootParent(any())).thenReturn(Mono.just(createdTask));
 
-        // When
-        TasksDto.TaskResponse result = taskService.createTask(email, request);
+        try (MockedStatic<SecurityUtils> utilities = mockStatic(SecurityUtils.class)) {
+            utilities.when(SecurityUtils::getAuthenticatedUserEmail).thenReturn(email);
 
-        // Then
-        assertEquals("CreateTaskID Test","1", result.getTaskId());
-        assertEquals("CreateTaskName Test","Task 1", result.getName());
-        verify(taskRepository, times(1)).createTaskWithRootParent(any());
+            // When
+            TasksDto.TaskResponse result = taskService.createTask(request);
+
+            // Then
+            assertEquals("CreateTaskID Test","1", result.getTaskId());
+            assertEquals("CreateTaskName Test","Task 1", result.getName());
+            verify(taskRepository, times(1)).createTaskWithRootParent(any());
+        }
     }
 
     @Test
@@ -166,20 +176,24 @@ class TaskServiceTest {
         Task createdTask = new Task("2", "Task 1", "Memo 1", LocalDateTime.now(), LocalDateTime.now().plusDays(1), 1, false);
         when(taskRepository.createTaskWithParent(any())).thenReturn(Mono.just(createdTask));
 
-        // When
-        TasksDto.TaskResponse result = taskService.createTask(email, request);
+        try (MockedStatic<SecurityUtils> utilities = mockStatic(SecurityUtils.class)) {
+            utilities.when(SecurityUtils::getAuthenticatedUserEmail).thenReturn(email);
 
-        // Then
-        assertEquals("CreateTaskID with ParentId Test","2", result.getTaskId());
-        assertEquals("CreateTaskName with ParentId Test","Task 1", result.getName());
-        verify(taskRepository, times(1)).createTaskWithParent(any());
+            // When
+            TasksDto.TaskResponse result = taskService.createTask(request);
+
+            // Then
+            assertEquals("CreateTaskID with ParentId Test","2", result.getTaskId());
+            assertEquals("CreateTaskName with ParentId Test","Task 1", result.getName());
+            verify(taskRepository, times(1)).createTaskWithParent(any());
+        }
     }
 
     @Test
     void updateTask_shouldReturnUpdatedTaskResponse_whenTaskExists() {
         // Given
         TasksDto.TaskUpdateRequest request = TasksDto.TaskUpdateRequest.builder()
-                .id("1")
+                .taskId("1")
                 .name("Updated Task")
                 .memo("Updated Memo")
                 .startDate(LocalDateTime.now())
@@ -209,7 +223,7 @@ class TaskServiceTest {
     void updateTask_shouldThrowTaskNotFoundException_whenTaskDoesNotExist() {
         // Given
         TasksDto.TaskUpdateRequest request = TasksDto.TaskUpdateRequest.builder()
-                .id("1")
+                .taskId("1")
                 .name("Updated Task")
                 .memo("Updated Memo")
                 .startDate(LocalDateTime.now())
@@ -230,19 +244,23 @@ class TaskServiceTest {
         // Given
         String email = "test@example.com";
         TasksDto.TaskDeleteRequest request = TasksDto.TaskDeleteRequest.builder()
-                .id("1")
+                .taskId("1")
                 .cascade(true)
                 .build();
         when(taskRepository.deleteTaskByIdWithCascade(email, "1")).thenReturn(Mono.just(3));
 
-        // When
-        TasksDto.TaskDeleteResponse result = taskService.deleteTask(email, request);
+        try (MockedStatic<SecurityUtils> utilities = mockStatic(SecurityUtils.class)) {
+            utilities.when(SecurityUtils::getAuthenticatedUserEmail).thenReturn(email);
 
-        // Then
-        assertEquals("Test","1", result.getId());
-        assertEquals("Test",3, result.getDeletedNodes());
-        verify(taskRepository, times(1)).deleteTaskByIdWithCascade(email, "1");
-        verify(taskRepository, never()).deleteTaskById(anyString(), anyString());
+            // When
+            TasksDto.TaskDeleteResponse result = taskService.deleteTask(request);
+
+            // Then
+            assertEquals("Test","1", result.getTaskId());
+            assertEquals("Test",3, result.getDeletedNodes());
+            verify(taskRepository, times(1)).deleteTaskByIdWithCascade(email, "1");
+            verify(taskRepository, never()).deleteTaskById(anyString(), anyString());
+        }
     }
 
     @Test
@@ -250,18 +268,22 @@ class TaskServiceTest {
         // Given
         String email = "test@example.com";
         TasksDto.TaskDeleteRequest request = TasksDto.TaskDeleteRequest.builder()
-                .id("1")
+                .taskId("1")
                 .cascade(false)
                 .build();
         when(taskRepository.deleteTaskById(email, "1")).thenReturn(Mono.just(1));
 
-        // When
-        TasksDto.TaskDeleteResponse result = taskService.deleteTask(email, request);
+        try (MockedStatic<SecurityUtils> utilities = mockStatic(SecurityUtils.class)) {
+            utilities.when(SecurityUtils::getAuthenticatedUserEmail).thenReturn(email);
 
-        // Then
-        assertEquals("Test","1", result.getId());
-        assertEquals("Test",1, result.getDeletedNodes());
-        verify(taskRepository, times(1)).deleteTaskById(email, "1");
-        verify(taskRepository, never()).deleteTaskByIdWithCascade(anyString(), anyString());
+            // When
+            TasksDto.TaskDeleteResponse result = taskService.deleteTask(request);
+
+            // Then
+            assertEquals("Test","1", result.getTaskId());
+            assertEquals("Test",1, result.getDeletedNodes());
+            verify(taskRepository, times(1)).deleteTaskById(email, "1");
+            verify(taskRepository, never()).deleteTaskByIdWithCascade(anyString(), anyString());
+        }
     }
 }
