22e4ef34a3216b8e384136bd8dc03621c2dab3b4
Haneol <haneolj@gmail.com>
2024-06-25 16:08:41 +0900

    [bugfix/EASY-53] fix: taskId가 root, trash, archive인 경우 bad request 발생하도록 예외처리

--- src/main/java/team/molu/edayserver/controller/TaskController.java
+++ src/main/java/team/molu/edayserver/controller/TaskController.java
@@ -1,6 +1,7 @@
 package team.molu.edayserver.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import team.molu.edayserver.domain.Task;
 import team.molu.edayserver.dto.TasksDto;
@@ -13,6 +14,17 @@ public class TaskController {
 
     public TaskController(TaskService taskService) { this.taskService = taskService; }
 
+    /**
+     * taskId가 유효한 지 체크 하는 private 함수
+     *
+     * @param taskId 체크할 ID, String
+     * @return 유효한 지 여부
+     */
+    private Boolean isValidTaskId(String taskId) {
+        String trimmedTaskId = taskId.trim().toLowerCase();
+        return !trimmedTaskId.equals("root") && !trimmedTaskId.equals("trash") && !trimmedTaskId.equals("archive");
+    }
+
     @GetMapping("/roots")
     public ResponseEntity<TasksDto.SearchTasksResponse> findRootTasks() {
         TasksDto.SearchTasksResponse tasks = taskService.findTaskByRoot();
@@ -20,65 +32,97 @@ public class TaskController {
     }
 
     @GetMapping("/{taskId}")
-    public ResponseEntity<TasksDto.TaskResponse> findTaskDetail(@PathVariable String taskId) {
-        TasksDto.TaskResponse tasks = taskService.findTaskById(taskId);
-        return ResponseEntity.ok(tasks);
+    public ResponseEntity<?> findTaskDetail(@PathVariable String taskId) {
+        if(!isValidTaskId(taskId)) {
+            return ResponseEntity.badRequest().body(taskId + " is not allowed.");
+        } else {
+            TasksDto.TaskResponse tasks = taskService.findTaskById(taskId);
+            return ResponseEntity.ok(tasks);
+        }
     }
 
     @GetMapping("/{taskId}/subtasks")
-    public ResponseEntity<TasksDto.SearchTasksResponse> findChildTasks(@PathVariable String taskId) {
-        TasksDto.SearchTasksResponse tasks = taskService.findSubtaskById(taskId);
-        return ResponseEntity.ok(tasks);
+    public ResponseEntity<?> findChildTasks(@PathVariable String taskId) {
+        if(!isValidTaskId(taskId)) {
+            return ResponseEntity.badRequest().body(taskId + " is not allowed.");
+        } else {
+            TasksDto.SearchTasksResponse tasks = taskService.findSubtaskById(taskId);
+            return ResponseEntity.ok(tasks);
+        }
     }
 
     @GetMapping("/{taskId}/routes")
-    public ResponseEntity<TasksDto.TaskRouteResponse> getTaskRoutesById(@PathVariable String taskId) {
-        TasksDto.TaskRouteResponse routes = taskService.getTaskRoutes(taskId);
-        return ResponseEntity.ok(routes);
+    public ResponseEntity<?> getTaskRoutesById(@PathVariable String taskId) {
+        if(!isValidTaskId(taskId)) {
+            return ResponseEntity.badRequest().body(taskId + " is not allowed.");
+        } else {
+            TasksDto.TaskRouteResponse routes = taskService.getTaskRoutes(taskId);
+            return ResponseEntity.ok(routes);
+        }
     }
 
     @PostMapping
-    public ResponseEntity<TasksDto.TaskResponse> createTask(@RequestBody TasksDto.TaskCreateRequest taskDto) {
-        TasksDto.TaskResponse task = taskService.createTask(taskDto);
-        return ResponseEntity.ok(task);
+    public ResponseEntity<?> createTask(@RequestBody TasksDto.TaskCreateRequest taskDto) {
+        if(!isValidTaskId(taskDto.getParentId())) {
+            return ResponseEntity.badRequest().body(taskDto.getParentId() + " is not allowed.");
+        } else {
+            TasksDto.TaskResponse task = taskService.createTask(taskDto);
+            return ResponseEntity.ok(task);
+        }
     }
 
     @PatchMapping
-    public ResponseEntity<TasksDto.TaskResponse> updateTask(@RequestBody TasksDto.TaskUpdateRequest taskDto) {
-        TasksDto.TaskResponse task = taskService.updateTask(taskDto);
-        return ResponseEntity.ok(task);
+    public ResponseEntity<?> updateTask(@RequestBody TasksDto.TaskUpdateRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else {
+            TasksDto.TaskResponse task = taskService.updateTask(taskDto);
+            return ResponseEntity.ok(task);
+        }
     }
 
     @PostMapping("/delete")
-    public ResponseEntity<TasksDto.TaskDeleteResponse> deleteTask(@RequestBody TasksDto.TaskDeleteRequest taskDto) {
-        TasksDto.TaskDeleteResponse taskDeleteResponse = taskService.deleteTask(taskDto);
-        return ResponseEntity.ok(taskDeleteResponse);
+    public ResponseEntity<?> deleteTask(@RequestBody TasksDto.TaskDeleteRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else {
+            TasksDto.TaskDeleteResponse taskDeleteResponse = taskService.deleteTask(taskDto);
+            return ResponseEntity.ok(taskDeleteResponse);
+        }
     }
 
     @GetMapping(value = "", params = {"startDate","endDate"})
     public ResponseEntity<TasksDto.SearchTasksResponse> findTasksByDate(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
-
     	TasksDto.SearchTasksResponse task = taskService.findTasksByDate(startDate, endDate);
         return ResponseEntity.ok(task);
     }
 
     @GetMapping("/all")
     public ResponseEntity<TasksDto.SearchTasksResponse> findAllTasks() {
-
     	TasksDto.SearchTasksResponse task = taskService.findAllTasks();
         return ResponseEntity.ok(task);
     }
 
     @PostMapping("/restore")
-    public ResponseEntity<TasksDto.TaskRestoreResponse> restoreTask(@RequestBody TasksDto.TaskRestoreRequest taskDto) {
-        TasksDto.TaskRestoreResponse taskRestoreResponse = taskService.restoreTask(taskDto);
-        return ResponseEntity.ok(taskRestoreResponse);
+    public ResponseEntity<?> restoreTask(@RequestBody TasksDto.TaskRestoreRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else if (!isValidTaskId(taskDto.getParentId())) {
+            return ResponseEntity.badRequest().body(taskDto.getParentId() + " is not allowed.");
+        } else {
+            TasksDto.TaskRestoreResponse taskRestoreResponse = taskService.restoreTask(taskDto);
+            return ResponseEntity.ok(taskRestoreResponse);
+        }
     }
 
     @DeleteMapping("/drop")
-    public ResponseEntity<TasksDto.TaskDeleteResponse> dropTask(@RequestBody TasksDto.TaskDeleteRequest taskDto) {
-        TasksDto.TaskDeleteResponse taskDropResponse = taskService.dropTask(taskDto);
-        return ResponseEntity.ok(taskDropResponse);
+    public ResponseEntity<?> dropTask(@RequestBody TasksDto.TaskDeleteRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else {
+            TasksDto.TaskDeleteResponse taskDropResponse = taskService.dropTask(taskDto);
+            return ResponseEntity.ok(taskDropResponse);
+        }
     }
 
     @DeleteMapping("/drop/all")
@@ -88,20 +132,36 @@ public class TaskController {
     }
 
     @PostMapping("/move")
-    public ResponseEntity<TasksDto.TaskMoveResponse> moveTask(@RequestBody TasksDto.TaskMoveRequest taskDto) {
-        TasksDto.TaskMoveResponse taskMoveResponse = taskService.moveTask(taskDto);
-        return ResponseEntity.ok(taskMoveResponse);
+    public ResponseEntity<?> moveTask(@RequestBody TasksDto.TaskMoveRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else if (!isValidTaskId(taskDto.getParentId())) {
+            return ResponseEntity.badRequest().body(taskDto.getParentId() + " is not allowed.");
+        } else {
+            TasksDto.TaskMoveResponse taskMoveResponse = taskService.moveTask(taskDto);
+            return ResponseEntity.ok(taskMoveResponse);
+        }
     }
 
     @PostMapping("/archive")
-    public ResponseEntity<TasksDto.TaskArchiveResponse> archiveTask(@RequestBody TasksDto.TaskArchiveRequest taskDto) {
-        TasksDto.TaskArchiveResponse taskArchiveResponse = taskService.archiveTask(taskDto);
-        return ResponseEntity.ok(taskArchiveResponse);
+    public ResponseEntity<?> archiveTask(@RequestBody TasksDto.TaskArchiveRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else {
+            TasksDto.TaskArchiveResponse taskArchiveResponse = taskService.archiveTask(taskDto);
+            return ResponseEntity.ok(taskArchiveResponse);
+        }
     }
 
     @PostMapping("/unarchive")
-    public ResponseEntity<TasksDto.TaskUnarchiveResponse> unarchiveTask(@RequestBody TasksDto.TaskUnarchiveRequest taskDto) {
-        TasksDto.TaskUnarchiveResponse taskUnarchiveResponse = taskService.unarchiveTask(taskDto);
-        return ResponseEntity.ok(taskUnarchiveResponse);
+    public ResponseEntity<?> unarchiveTask(@RequestBody TasksDto.TaskUnarchiveRequest taskDto) {
+        if(!isValidTaskId(taskDto.getTaskId())) {
+            return ResponseEntity.badRequest().body(taskDto.getTaskId() + " is not allowed.");
+        } else if (!isValidTaskId(taskDto.getParentId())) {
+            return ResponseEntity.badRequest().body(taskDto.getParentId() + " is not allowed.");
+        } else {
+            TasksDto.TaskUnarchiveResponse taskUnarchiveResponse = taskService.unarchiveTask(taskDto);
+            return ResponseEntity.ok(taskUnarchiveResponse);
+        }
     }
 }
