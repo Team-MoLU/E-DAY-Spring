4e44151203abc0774cae8496452b3aa1a103351a
Haneol <haneolj@gmail.com>
2024-07-09 17:42:40 +0900

    [develop] fix: 쿠키 생성시 httpOnly 옵션 적용(2)

--- src/main/java/team/molu/edayserver/security/oauth2/CustomJWTSuccessHandler.java
+++ src/main/java/team/molu/edayserver/security/oauth2/CustomJWTSuccessHandler.java
@@ -81,7 +81,7 @@ public class CustomJWTSuccessHandler extends SimpleUrlAuthenticationSuccessHandl
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60*60*60);
-//        cookie.setSecure(true);  // HTTPS일 때 사용
+        cookie.setSecure(true);  // HTTPS일 때 사용
         cookie.setPath("/");
 //        cookie.setHttpOnly(true);
         log.info("Created Cookie: name={}, value={}", key, value);
