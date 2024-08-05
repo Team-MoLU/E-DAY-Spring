bc92baec27b8337fa817b15b9e7776c8b8efdc60
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2024-08-06 00:34:00 +0900

    Update SecurityConfig.java

--- src/main/java/team/molu/edayserver/config/SecurityConfig.java
+++ src/main/java/team/molu/edayserver/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins("http://localhost:3000", "https://eday.site");
+                        configuration.setAllowedOrigins(Collections.singletonList(clientUrl));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
